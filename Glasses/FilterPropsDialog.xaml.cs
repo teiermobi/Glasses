57bcee96655547e28cabad9d8facfa56a15ba152
teiermobi <tobi.meier89@gmail.com>
2017-06-10 17:00:07 +0200

    ...

--- Glasses/FilterPropsDialog.xaml.cs
+++ Glasses/FilterPropsDialog.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Web;
 
 namespace Glasses
 {
@@ -25,35 +26,73 @@ namespace Glasses
             main = this;
             comboBoxFilter.SelectedIndex = 0;
             OffsetDisp.Text = "3";
-           
 
 
-          //  IEnumerable<TextBox> TextBoxes = this.rasterGrid.Children.OfType<TextBox>();
 
-          
-        //    for (int b = 0; b <= FilterGlass.main.Mask.GetLength(0) * FilterGlass.main.Mask.GetLength(1); b++)
-        //    {
-        //        TextBox tb = new TextBox();
-                
-        //        for (int i = 0; i < FilterGlass.main.Mask.GetLength(0); i++)
-        //        {
-        //            for (int j = 0; j < FilterGlass.main.Mask.GetLength(1); j++)
-        //            {
-        //                double s = FilterGlass.main.Mask[i +1, j+1];
+            //IEnumerable<TextBox> TextBoxes = this.rasterGrid.Children.OfType<TextBox>();
+
+
+            //Grid myGrid = new Grid();
+            //myGrid.Width = 250;
+            //myGrid.Height = 100;
+            //myGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            //myGrid.VerticalAlignment = VerticalAlignment.Top;
+            //myGrid.ShowGridLines = true;
+
+            //// Define the Columns
+            //ColumnDefinition colDef1 = new ColumnDefinition();
+            //ColumnDefinition colDef2 = new ColumnDefinition();
+            //ColumnDefinition colDef3 = new ColumnDefinition();
+            //myGrid.ColumnDefinitions.Add(colDef1);
+            //myGrid.ColumnDefinitions.Add(colDef2);
+            //myGrid.ColumnDefinitions.Add(colDef3);
+
+            //// Define the Rows
+            //RowDefinition rowDef1 = new RowDefinition();
+            //RowDefinition rowDef2 = new RowDefinition();
+            //RowDefinition rowDef3 = new RowDefinition();
+            //myGrid.RowDefinitions.Add(rowDef1);
+            //myGrid.RowDefinitions.Add(rowDef2);
+            //myGrid.RowDefinitions.Add(rowDef3);
+            //groupBox.Content = myGrid;
+
+                for (int i = 0; i < FilterGlass.main.Mask.GetLength(0); i++ )
+                {
+                    for (int j = 0; j < FilterGlass.main.Mask.GetLength(1); j++ )
+                    {
+                        double s = FilterGlass.main.Mask[i, j];
                        
-        //                    tb.Text = s.ToString();
-        //                    this.rasterGrid.Children.Add(tb);
+                    
+                        for(int b = 0; b <= 8; b++)
+                        {
+                            //this.RegisterName("textBox" + b, Textbox).Text = s.ToString();
+                            //tb.Text = s.ToString();
+                         }
+                      
+                        
+                        //myGrid.Children.Add(tb);
 
+                            
+                        // this.rasterGrid.Children.Add(tb);
+                        
+                    }
 
-
-        //            }
-
-        //        }
-        //    }
+                }
 
         }
 
+
+
         internal static FilterPropsDialog main;
+
+        public class TextBox
+        {
+            public TextBox()
+            {
+               
+            }
+        }
+        
 
         public class ComboboxItem
         {
@@ -91,8 +130,8 @@ namespace Glasses
 
         public double Textbox
         {
-            get { return Convert.ToDouble(textBox.Text); }
-            set { textBox.Text = value.ToString(); }
+            get { return Convert.ToDouble(textBox1.Text); }
+            set { textBox1.Text = value.ToString(); }
         }
 
 
