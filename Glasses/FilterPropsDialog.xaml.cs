27aa25577c48db33ebddda03b68d42b34f2ca692
lukas <lukas@WINDOWS-DELL-01>
2017-06-16 12:25:17 +0200

    FilterPropsDynamische textboxen

--- Glasses/FilterPropsDialog.xaml.cs
+++ Glasses/FilterPropsDialog.xaml.cs
@@ -64,6 +64,10 @@ namespace Glasses
                         double value = FilterGlass.main.Mask[i, j];
                         TextBox tb = new TextBox();
                         tb.Text = value.ToString();
+                        tb.Margin = new Thickness(5, 5, 5, 5);
+                        tb.HorizontalContentAlignment = HorizontalAlignment.Center;
+                        tb.VerticalContentAlignment = VerticalAlignment.Center;
+                        // tb.Name = i+"_"+j;
                         rasterGrid.Children.Add(tb);
                     }
                 }
@@ -96,6 +100,7 @@ namespace Glasses
                 intOld = comboBoxFilter.SelectedIndex;
                 GenerateMatrix(MaskLength);
                 Std_KMP_Glasses.main.canvasCanvas.InvalidateVisual();
+                InvalidateVisual();
             }
 
             else if (comboBoxFilter.SelectedItem.ToString() == "Kanten")
@@ -104,6 +109,7 @@ namespace Glasses
                 intOld = comboBoxFilter.SelectedIndex;
                 GenerateMatrix(MaskLength);
                 Std_KMP_Glasses.main.canvasCanvas.InvalidateVisual();
+                InvalidateVisual();
             }
 
         }
@@ -117,6 +123,7 @@ namespace Glasses
 
         private void Offset_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
+
             if (e.NewValue > e.OldValue)
             {
                 int old = int.Parse(OffsetDisp.Text);
