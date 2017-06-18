fc2b7dbd90af7cd2417823a8e9328b723feab985
FrostigerFinger <martin.kleber@mail.de>
2017-06-18 20:46:12 +0200

    Einfaches Drehen in Waterglass (geht noch nicht, getPixel und setPixel sind nicht unabhängig!)

--- Glasses/WaterGlass.cs
+++ Glasses/WaterGlass.cs
@@ -40,7 +40,7 @@ namespace Glasses
             timmy.Tick += Timmy_Tick;
             this.DistortionLimit = 80.0;
             this.Distortion = 0.0;
-            this.DistortionDelta = 2.0;
+            this.DistortionDelta = 0.0;
             this.WaveDensity = 0.1;
 
             if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this)) timmy.Start();
@@ -150,16 +150,21 @@ namespace Glasses
 
             else if (WaterType == WaterGlassType.Strudel)
             {
+                /*
                 for (int j = 0; j < sY; j++)
                 {
                     for (int i = 0; i < sX; i++)
                     {
-                        double ir = ((2.0 * i) / m) - 1;
-                        double jr = ((2.0 * j) / n) - 1;
-                        double l = Math.Sqrt(Math.Pow(ir, 2.0) + Math.Pow(jr, 2.0));
+                       // double ir = i - m / 2.0;
+                       // double jr = n / 2.0 - j;
+                          double ir = ((2.0 * i) / m) - 1;
+                          double jr = ((2.0 * j) / n) - 1;
+                          double l = Math.Sqrt(Math.Pow(ir, 2.0) + Math.Pow(jr, 2.0));
                         //double winkel = Math.Atan2(oy + j, ox + i);
-                        double winkel = Math.Atan2( j-(n/2.0), i-(m/2.0));
+                        //double winkel = Math.Atan2( j-(n/2.0), i-(m/2.0));
+                        double winkel = Math.Atan2(jr, ir);
 
+                        
                         if (l < 1.0)
                         {
                             iorg = (ox + i) + (int)(0.5 * m * (1 + l * Math.Cos((winkel + 0.1 * (l - 1) * Distortion))));
@@ -167,15 +172,59 @@ namespace Glasses
                         }
                         else
                         {
-                            iorg = (int)(0.5 * m * (1 + l * Math.Cos(winkel)));
-                            jorg = (int)(0.5 * n * (1 + l * Math.Sin(winkel)));
+                            iorg = (ox + i) + (int)(0.5 * m * (1 + l * Math.Cos(winkel)));
+                            jorg = (oy + j) + (int)(0.5 * n * (1 + l * Math.Sin(winkel)));
                         }
+                        
 
                         c = painting.GetPixel(iorg, jorg);
 
                         painting.SetPixel(ox + i, oy + j, c);
                     }
                 }
+                */
+
+                //Einfaches Drehen um einen festen Winkel
+
+                //Hälfte der kleineren Dimension wird als Radius definiert 
+                int radius = m < n ? (int)(m / 2.0) : (int)(n / 2.0);
+
+                //Internes intuitives Koordinatensystem
+                for ( int y = radius; y > -radius; y--)
+                {
+                    for ( int x = -radius; x < radius; x++)
+                    {
+                        if ( x*x + y*y <= radius*radius ) //Pixel im Kreis?
+                        {
+                            double r = Math.Sqrt(x * x + y * y); //Abstand vom Mittelpunkt (einfach darstellbar durch internes Koord-syst.
+                            double alpha = Math.Atan2(y, x); //Winkel zum Mittelpunkt
+                            double deg = (alpha * 180.0) / Math.PI;
+                            deg += 90.0; //Drehwinkel
+                            alpha = (deg * Math.PI) / 180.0;
+
+                            //Ursprünge der neuen Farben
+                            int newY = (int)(Math.Floor(r * Math.Sin(alpha)));
+                            int newX = (int)(Math.Floor(r * Math.Cos(alpha)));
+
+                            //Abholen der Farbe im globalen Koordianatensystem
+                            c = painting.GetPixel(ox + (int)(m / 2.0) + newX, oy + (int)(n/2.0) - newY);
+
+
+                            //Untere Hälfte sollte funktionieren (tut sie aber nicht)
+                            if (y < 0)
+                            {
+                                painting.SetPixel(ox + (int)(m / 2.0) + x, oy + (int)(n / 2.0) - y, c);
+                            }
+                            else
+                            {      
+                                //Obere Hälfte wird absichtlich einfarbig gemacht
+                                painting.SetPixel(ox + (int)(m / 2.0) + x, oy + (int)(n / 2.0) - y, Colors.Violet);
+                            }
+                            // => GetPixel greift auf zuvor gesetzte Farbwerte zu, statt auf die Ursprungswerte, deep copy des BitmapEditors nötig, aber wie??
+                        }
+                    }
+                }
+
             }
 
 
